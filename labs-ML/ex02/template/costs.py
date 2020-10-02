80c940766b991015f2d0bcf2bace55fab8c2e982
Quentin Muller <quentin.muller@epfl.ch>
2020-10-02 14:17:49 +0200

    ADA Homework 1

--- labs-ML/ex02/template/costs.py
+++ labs-ML/ex02/template/costs.py
@@ -1,13 +1,18 @@
 # -*- coding: utf-8 -*-
 """Function used to compute the loss."""
 
-def compute_loss(y, tx, w):
+def compute_loss(y, tx, w, mae=False):
     """Calculate the loss.
 
     You can calculate the loss using mse or mae.
     """
     loss = 0
     txw = np.dot(tx, w)
-    for i in range(y.shape[0]) :
-        loss += (y[i] - txw[i])**2
+    if(not mae) :
+        for i in range(y.shape[0]) :
+            loss += (y[i] - txw[i])**2
+    else :
+        e = np.absolute(y - txw)
+        for i in range(y.shape[0]) :
+            loss += e[i]
     return loss / (2*y.shape[0])
--- labs-ML/ex03/template/build_polynomial.py
+++ labs-ML/ex03/template/build_polynomial.py
@@ -6,10 +6,8 @@ import numpy as np
 
 def build_poly(x, degree):
     """polynomial basis functions for input data x, for j=0 up to j=degree."""
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # polynomial basis function: TODO
-    # this function should return the matrix formed
-    # by applying the polynomial basis to the input data
-    # ***************************************************
-    raise NotImplementedError
+    poly = np.zeros((x.shape[0], degree + 1))
+    for i in range(degree + 1) :
+        for j in range(x.shape[0]) :
+            poly[j, i] = x[j] ** i
+    return poly
